4fa81494fb2fa06403d21648572b7e025af673a1
nosimk <konchellahnosim@gmail.com>
2025-03-03 09:11:06 +0300

    second commit

--- app/models.py
+++ app/models.py
@@ -7,6 +7,6 @@ class Wardrobe(models.Model):
     color = models.CharField(max_length=20)
     size = models.CharField(max_length=20)
     season = models.CharField(max_length=20)
-    date = models.DateField()
+    date = models.CharField(max_length=20)
     def __str__(self):
         return self.name
